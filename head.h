3e82e7ad3203411706789350278acf577a383d79
alpaca <lpc2022@163.com>
2021-04-19 12:59:01 +0800

    "First commit to github"

---
+++ head.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+void test() {
+	printf("Hello world!\n");
+}
+
+#endif
---
+++ test.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include "head.h"
+
+int main()"{
+	test();
+	return 0;
+}
